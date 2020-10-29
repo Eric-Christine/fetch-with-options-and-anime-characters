59279b96146a6bee4291e965997e56c179db8f31
LimitToInfinity <21stLimitless@gmail.com>
2020-10-29 10:03:51 -0600

    Adds 3 Character seeds.

--- anime_api/db/seeds.rb
+++ anime_api/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Character.create(
+  name: "Goku",
+  image: "https://vignette.wikia.nocookie.net/character-level/images/3/34/SSJ_Goku_FighterZ_Render.png/revision/latest/scale-to-width-down/340?cb=20200405202522",
+  goofiness: 8
+)
+
+Character.create(
+  name: "Ichigo Kurosaki",
+  image: "https://static.tvtropes.org/pmwiki/pub/images/bbs_ichigo.png",
+  goofiness: 5
+)
+
+Character.create(
+  name: "All Might",
+  image: "https://static2.cbrimages.com/wordpress/wp-content/uploads/2019/10/My-Hero-Academia-All-Might-Header.jpg",
+  goofiness: 11
+)
